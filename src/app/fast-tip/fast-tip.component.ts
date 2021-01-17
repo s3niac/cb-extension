e73b9b018ebd07b6a47220f7671e9c2438ff0724
s3niac <s3niac@yahoo.com>
2021-01-17 23:51:19 +0100

    undo send token pattern rework

--- src/app/fast-tip/fast-tip.component.ts
+++ src/app/fast-tip/fast-tip.component.ts
@@ -11,22 +11,25 @@ export class FastTipComponent {
   fastTipGroup: FormGroup;
   tipsPatternControl: FormControl;
   patternRepeatControl: FormControl;
+  tipBombsControl: FormControl;
 
   constructor(private tipService: TipService,
               formBuilder: FormBuilder) {
     this.tipsPatternControl = formBuilder.control('1 1 1 5 1 1 5');
     this.patternRepeatControl = formBuilder.control(1);
+    this.tipBombsControl = formBuilder.control('');
     this.fastTipGroup = formBuilder.group({
       pattern: this.tipsPatternControl,
       repeat: this.patternRepeatControl,
+      bombs: this.tipBombsControl,
     });
   }
 
   get overallTips(): number {
-    return this.tipService.tipPatternSum(this.tipsPatternControl.value, this.patternRepeatControl.value);
+    return this.tipService.tipPatternSum(this.tipsPatternControl.value, this.patternRepeatControl.value, this.tipBombsControl.value);
   }
 
   sendTips(): void {
-    this.tipService.tipPattern(this.tipsPatternControl.value, this.patternRepeatControl.value);
+    this.tipService.tipPattern(this.tipsPatternControl.value, this.patternRepeatControl.value, this.tipBombsControl.value);
   }
 }
